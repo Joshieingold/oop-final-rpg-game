da1998d40136a5d153f4229d95351c615aaf883e
josh <joshlynch2001@gmail.com>
2026-03-20 13:14:19 -0300

    added fight bones

---
+++ GameLibrary/Fight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Fight
+    {
+        private Fighter attacker { get; set; }
+        private Fighter defender { get; set; }
+        private void OnFinsh()
+        {
+            return; 
+        }
+        private void StartFight()
+        {
+            return;
+        }
+    }
+}
