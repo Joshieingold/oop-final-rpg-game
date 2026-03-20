d9e3823689f8beac1843e87123f629df18997ce0
josh <joshlynch2001@gmail.com>
2026-03-20 13:11:32 -0300

    added game state

---
+++ GameLibrary/State/GameState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.State
+{
+    public enum GameState 
+    {
+        Battle,
+        Shop,
+        Defeat,
+        Victory 
+    }
+}
