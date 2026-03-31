156649441b37c98d8ad5647fa3a623d5101bd70b
josh <joshlynch2001@gmail.com>
2026-03-31 14:40:13 -0300

    added all test cases for enemies and working on player

--- GameLibrary/Entities/Player.cs
+++ GameLibrary/Entities/Player.cs
@@ -6,8 +6,25 @@ namespace Core.Entities
     public sealed class Player : Fighter
     {
         private bool _isMale;
+        private string _name;
         public int Money { get; set; }
         public string PlayerSprite { get; private set; } // this is determined by their gender
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value == "")
+                {
+                    throw new InvalidDataException();
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+
+        }
         public bool IsMale
         {
             get { return _isMale; }
